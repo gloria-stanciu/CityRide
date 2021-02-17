2e1f114406ad034ca9f47f3cae2f50d61645c90a
zs <Z>
2021-02-17 10:04:26 +0200

    Test

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import express from 'express'
 const app = express();
 const port = 3000;
 app.get('/', (req, res) => {
-    res.json({message: 'It works!'});
+    res.json({message: 'This test is working!'});
 });
 app.listen(port, ()=>{
     return console.log(`App is listening on port ${port}`);
