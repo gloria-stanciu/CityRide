94df9f665344946f6535e7041bea756b48622456
zs <Z>
2021-03-17 23:52:23 +0200

    Added docker-compose file

--- Dockerfile
+++ Dockerfile
@@ -8,6 +8,8 @@ RUN npm install && npm run compile
 
 ENV HOST=0.0.0.0
 
+ENV NODE_ENV=production
+
 EXPOSE 3000
 
-CMD [ "node", "dist" ]
+CMD [ "node", "dist" ] 
--- src/knexfile.ts
+++ src/knexfile.ts
@@ -4,11 +4,18 @@ require('dotenv').config({ path: '../.env' })
 // require('ts-node/register');
 
 import {knexSnakeCaseMappers} from 'objection'
+
+var connString;
+
+if(process.env.NODE_ENV === 'production'){
+    connString = process.env.DATABASE_URL
+}
+
 export default {
     client: 'postgresql',
-    connection: {
+    connection: connString || {
         connectionString : process.env.DATABASE_URL,
-        ssl: {rejectUnauthorized: false},
+        ssl: {rejectUnauthorized: false}
     },
     migrations: {    
         tableName: 'knex_migrations',
