04b1ceb1eb3e04a5280bf37aba2b0c52eb7a30b7
zs <Z>
2021-03-31 01:08:29 +0300

    Fixed db connection string

--- src/knexfile.ts
+++ src/knexfile.ts
@@ -8,21 +8,21 @@ import { knexSnakeCaseMappers } from 'objection'
 var connString
 
 if (process.env.NODE_ENV === 'production') {
-  // connString = process.env.DATABASE_URL
   connString = process.env.DATABASE_URL
 }
 
 export default {
   client: 'pg',
-  connection: connString || process.env.DATABASE_URL,
+  connection: connString || {
+    connectionString: process.env.DATABASE_URL,
+    ssl: { rejectUnauthorized: false },
+  },
 
   // host: hostName || process.env.RDS_HOSTNAME,
   // user: process.env.RDS_USERNAME,
   // password: process.env.RDS_PASSWORD,
   // port: process.env.RDS_PORT,
   // dbname: process.env.RDS_DB_NAME,
-  ssl: { rejectUnauthorized: false },
-  // connectTimeout: 90000,
   migrations: {
     tableName: 'knex_migrations',
     directory: './migrations',
