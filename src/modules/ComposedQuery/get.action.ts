6535c22fad30f5d4c3b6b0c2107166a77c16eaed
zs <Z>
2021-05-04 18:23:51 +0300

    Modified filter by vehicle route

--- src/modules/ComposedQuery/get.action.ts
+++ src/modules/ComposedQuery/get.action.ts
@@ -3,14 +3,11 @@ import { Route } from '../../models/index'
 
 async function filterByVehicles(req: Request, res: Response) {
   try {
-    const route = await Route.query()
+    await Route.query()
       .where('type', req.params.type)
       .withGraphFetched(
         'trip(defaultSelects).[shape.[shapePoint(defaultSelects)], stopTime(defaultSelects).[stop(defaultSelects)]]'
       )
-    if (route === undefined)
-      return res.status(404).send({ message: 'Row not found' })
-    else return res.status(200).send(route)
   } catch (err) {
     console.log(err)
     res.status(500).send(err)
