52f1d50f6600f352c72460cd1ee53ff4905befdd
Gloria Stanciu <gloriastanciu@gmail.com>
2021-05-17 21:20:16 +0300

    Changed route for get shape points by shpa id

--- src/modules/ShapePoint/shapePoint.routing.ts
+++ src/modules/ShapePoint/shapePoint.routing.ts
@@ -10,7 +10,7 @@ shapePointRouter.get('/', getAll)
 shapePointRouter.post('/', create)
 // shapePointRouter.get('/:id', getById)
 shapePointRouter.get('/:lat/:long', getByLatLong)
-shapePointRouter.get('/:shapeId', getByShapeId)
+shapePointRouter.get('/shape/:shapeId', getByShapeId)
 shapePointRouter.put('/:lat/:long', update)
 shapePointRouter.delete('/:id', remove)
 
